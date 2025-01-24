b3597c7f67f687fdfa24d8922b83571eeeb57f61
Varun Arora <aroravaruncode@gmail.com>
2025-01-24 18:11:51 +0530

    add join room feature and typesafety

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,17 +1,35 @@
 import { WebSocket, WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({port: 3000});
-WebSocket
-wss.on('connection', (socket) => {
-    console.log("connection established")
-    socket.on("open", () => {
-        socket.send("open");
-    })
-    socket.on("message", (e) => {
-        wss.clients.forEach((client) => {
-            if(client !== socket){
-                client.send(e.toString())
+const wss = new WebSocketServer({ port: 3000 });
+const sockets = new Map<WebSocket, string>();
+
+wss.on("connection", (socket) => {
+  console.log("connection established");
+  socket.on("message", (e) => {
+    const { type, payload } = JSON.parse(e.toString());
+    switch (type) {
+
+      case "join":
+        sockets.set(socket, payload.roomId);
+        socket.send("Room joined");
+        break;
+
+      case "chat":
+        const currentRoom = sockets.get(socket)
+        sockets.forEach((roomId, ws) => {
+            if(currentRoom === roomId) {
+                ws.send(payload.message);
             }
         })
-    })
-})
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  socket.on("close", () => {
+    sockets.delete(socket);
+  })
+});
+
--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,41 +1,55 @@
-import { useEffect, useRef, useState } from 'react'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
-  const inputRef = useRef()
-  const [socket, setSocket] = useState()
-  const [messages, setMessages] = useState([])
-  const [message, setMessage] = useState("")
-  
-  const handleClick = () => {
-    socket.send(inputRef.current.value);
-  }
-  useEffect(() => {
-    const ws = new WebSocket("ws://localhost:3000")
-    ws.onmessage = (event) => {
-      setMessage(event.data)
-    }
-    setSocket(ws)
-  },[])
+  const inputRef = useRef<HTMLInputElement>(null);
+  const roomIdRef = useRef<HTMLInputElement>(null);
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const [messages, setMessages] = useState<string[]>([]);
 
+  const handleJoinRoom = () => {
+    if(socket && roomIdRef.current){
+      socket.send(JSON.stringify({
+        type: "join",
+        payload: {
+          roomId: roomIdRef.current.value
+        }
+      }));
+    }
+  };
+  const handleClick = () => {
+    if(socket && inputRef.current) {
+      socket.send(JSON.stringify({
+        type: "chat",
+        payload: {
+          message: inputRef.current.value
+        }
+      }));
+    }
+  };
   useEffect(() => {
-    setMessages([...messages, message])
-    setMessage("")
-  },[message])
+    const ws = new WebSocket("ws://localhost:3000");
+    ws.onmessage = (event) => {
+      setMessages((m: string[]) => [...m, event.data]);
+    };
+    setSocket(ws);
+
+    return () => ws.close()
+  }, []);
 
   return (
     <>
+      <input ref={roomIdRef} type="text" placeholder="message..." />
+      <button onClick={handleJoinRoom}>Join Room</button>
       <div>
         {messages.map((message, index) => {
-          return <div key={index}>
-            {message}
-          </div>
+          return <div key={index}>{message}</div>;
         })}
       </div>
-      <input ref={inputRef} type="text" placeholder='message...'/>
+      <input ref={inputRef} type="text" placeholder="message..." />
       <button onClick={handleClick}>Send</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
