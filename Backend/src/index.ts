5a1e4bb5e1952caef1dd40d74c7be4411d886123
Varun Arora <aroravaruncode@gmail.com>
2025-02-08 23:46:23 +0530

    change backend

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,6 +1,9 @@
+import express from "express";
 import { WebSocket, WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({ port: 3000 });
+const app = express();
+const httpServer = app.listen(3000);
+const wss = new WebSocketServer({ server: httpServer });
 const sockets = new Map<WebSocket, string>();
 
 wss.on("connection", (socket) => {
@@ -8,19 +11,34 @@ wss.on("connection", (socket) => {
   socket.on("message", (e) => {
     const { type, payload } = JSON.parse(e.toString());
     switch (type) {
-
       case "join":
         sockets.set(socket, payload.roomId);
-        socket.send("Room joined");
+        sockets.forEach((roomId, ws) => {
+          if (payload.roomId === roomId) {
+            ws.send(
+              {
+                roomId: payload.roomId,
+                username: payload.username,
+                message: "Room joined",
+              }.toString()
+            );
+          }
+        });
         break;
 
       case "chat":
-        const currentRoom = sockets.get(socket)
+        const currentRoom = sockets.get(socket);
         sockets.forEach((roomId, ws) => {
-            if(currentRoom === roomId) {
-                ws.send(payload.message);
-            }
-        })
+          if (currentRoom === roomId) {
+            ws.send(
+              {
+                roomId: payload.roomId,
+                username: payload.username,
+                message: payload.message,
+              }.toString()
+            );
+          }
+        });
         break;
 
       default:
@@ -30,6 +48,5 @@ wss.on("connection", (socket) => {
 
   socket.on("close", () => {
     sockets.delete(socket);
-  })
+  });
 });
-
